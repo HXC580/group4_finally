50bb2cb04b6630c7f7f6e7e3a1d5243e68c41be7
HXC580 <59186594+HXC580@users.noreply.github.com>
2020-04-17 16:48:10 +0800

    Update StaticPeram.java

--- src/main/java/com/example/group4/web/controller/test/StaticPeram.java
+++ src/main/java/com/example/group4/web/controller/test/StaticPeram.java
@@ -12,8 +12,8 @@ public class StaticPeram {
     final static String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
     final static String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
     // 替换成你的AK (产品密)
-    final static String accessKeyId = "LTAI4Fv2qB6pyjD4g87eE73p";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
-    final static String accessKeySecret = "ZEtDAE4ebo6ukiODc1keU7zX4nHOda";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
+    final static String accessKeyId = "";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
+    final static String accessKeySecret = "";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
     // 必填:短信签名-可在短信控制台中找到
     final static String SignName = "校园卡管理系统CC";    // 阿里云配置你自己的短信签名填入
     // 必填:短信模板-可在短信控制台中找到
